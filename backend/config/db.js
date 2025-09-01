aaeaf5f4b499a885ba47a2d231cd743fca152009
vince Mouko <idnovation2014@gmail.com>
2025-09-01 20:31:41 +0200

    ....

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2');
 
+
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER || 'vinisys',
-  password: process.env.MYSQL_PASSWORD || 'password123',
-  database: process.env.MYSQL_DATABASE || 'vinisys',
-  
+  host: 'localhost',
+  port: 8889,  // Port MySQL de MAMP
+  user: 'root',
+  password: 'root',
+  database: 'newergo',
   multipleStatements: true, // 👉 autorise plusieurs requêtes
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+ 
 // Test de connexion au démarrage
 const testConnection = async () => {
   try {
